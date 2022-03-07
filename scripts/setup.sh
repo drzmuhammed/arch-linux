5cdf51aa3681c1db0cd9e0da9ef8da61cbbb64b7
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-07 18:21:01 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-echo -ne "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+sed 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 echo -ne "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo -ne "us" >> /etc/vconsole.conf
@@ -27,10 +27,13 @@ echo -ne "
 -------------------------------------------------------------------------
 
 "
-#sed
-#mkinitcpio -p linux
-#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
-#grub-mkconfig -o /boot/grub/grub.cfg
-#sed
-#grub-mkconfig -o /boot/grub/grub.cfg
+sed 's/MODULES=()/MODULES=(btrfs)/g' /etc/mkinitcpio.conf
+sed 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect keyboard modconf block encrypt filesystems resume fsck)/g' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+$ENCRYPTED_PARTITION_UUID
+GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet cryptdevice=UUID=98c5d2ed-54cf-468e-9116-a2065500beae:cryptedsda2 root=/dev/mapper/cryptedsda2"
+sed 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet cryptdevice=UUID=98c5d2ed-54cf-468e-9116-a2065500beae:cryptedsda2 root=/dev/mapper/cryptedsda2"/g' /etc/mkinitcpio.conf
+grub-mkconfig -o /boot/grub/grub.cfg
 
