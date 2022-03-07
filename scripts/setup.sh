93fa0bc12e2d24f6d775352bc2b50aae6215c370
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-07 21:29:06 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -79,6 +79,7 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --re
 grub-mkconfig -o /boot/grub/grub.cfg
 sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:cryptedsda2 root=/dev/mapper/cryptedsda2 %g" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
+mkinitcpio -p linux
 
 echo -ne "
 
@@ -98,3 +99,66 @@ chmod 600 /swap/swapfile
 swapon /swap/swapfile lo
 echo -ne "/swap/swapfile none swap defaults 0 0" >> /etc/fstab
 
+echo -ne "
+-------------------------------------------------------------------------
+                            configuring firewall
+-------------------------------------------------------------------------
+
+"
+ufw disable
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 853/tcp
+ufw allow 853/udp
+ufw allow 443/tcp
+ufw allow 443/udp
+ufw enable
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Enabling Essential Services
+-------------------------------------------------------------------------
+
+"
+systemctl enable acpid.service
+echo -ne "  enabling acpid.service"
+systemctl enable apparmor.service
+echo -ne "  enabling  apparmor.service"
+systemctl enable auditd.service
+echo -ne "  enabling  auditd.service"
+systemctl enable bluetooth.service
+echo -ne "  enabling  bluetooth.service"
+systemctl enable cpupower.service
+echo -ne "  enabling  cpupower.service"
+systemctl enable fail2ban.service
+echo -ne "  enabling  fail2ban.service"
+systemctl enable fstrim.timer
+echo -ne "  enabling  fstrim.timer"
+systemctl enable NetworkManager.service
+echo -ne "  enabling  NetworkManager.service"
+systemctl enable reflector.timer
+echo -ne "  enabling reflector.timer"
+systemctl enable systemd-remount-fs.service
+echo -ne "  enabling systemd-remount-fs.service"
+systemctl enable systemd-resolved.service
+echo -ne "  enabling  systemd-resolved.service"
+systemctl enable systemd-timesyncd.service
+echo -ne "  enabling  systemd-timesyncd.service"
+systemctl enable thermald.service
+echo -ne "  enabling  thermald.service"
+systemctl enable tlp.service
+echo -ne "  enabling  tlp.service"
+systemctl enable ufw.service
+echo -ne "  enabling  ufw.service"
+
+echo -ne "
+-------------------------------------------------------------------------
+                   User Setup
+-------------------------------------------------------------------------
+
+"
+$(cat $CONFIGS_DIR/user-name.txt)
+$(cat $CONFIGS_DIR/user-password.txt)
+sed 's/#%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' visudo
+
+umount -a
