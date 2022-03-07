6ebe9a937e9c7ec012b76a37212a57415972ee87
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-07 10:50:00 +0530

    Update 1-arch-base-system.sh

--- scripts/1-arch-base-system.sh
+++ scripts/1-arch-base-system.sh
@@ -8,3 +8,27 @@ echo "${machine_name}" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ${machine_name}.localdomain ${machine_name}" >> /etc/hosts
+
+
+echo -ne "
+
+-------------------------------------------------------------------------
+             installing core linux packages
+-------------------------------------------------------------------------
+
+"
+pacman -S --needed - < $PACKAGES_DIR/pacman-base
+
+echo -ne "
+
+-------------------------------------------------------------------------
+             installing and configuring grub
+-------------------------------------------------------------------------
+
+"
+sed
+mkinitcpio -p linux
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+sed
+grub-mkconfig -o /boot/grub/grub.cfg
