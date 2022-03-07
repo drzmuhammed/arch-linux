bac2d84d6d69d8b702dfe5db2d56f318172d79bf
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-07 16:59:37 +0530

    Update and rename 1-arch-base-system.sh to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo -ne "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "us" >> /etc/vconsole.conf
-echo "${machine_name}" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 ${machine_name}.localdomain ${machine_name}" >> /etc/hosts
+echo -ne "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo -ne "us" >> /etc/vconsole.conf
+echo -ne "${machine_name}" >> /etc/hostname
+echo -ne "127.0.0.1 localhost" >> /etc/hosts
+echo -ne "::1       localhost" >> /etc/hosts
+echo -ne "127.0.1.1 ${machine_name}.localdomain ${machine_name}" >> /etc/hosts
 
 
 echo -ne "
