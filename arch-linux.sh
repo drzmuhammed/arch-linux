a3620c37e806e0bdebeaaa7fe095501694e6fad3
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-07 10:34:43 +0530

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -14,6 +14,7 @@ set -a
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
 CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
+PACKAGES_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/pkgs
 set +a
 
 touch $CONFIGS_DIR/setup.conf
