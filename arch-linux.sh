f1aaa9d232a260749f6445831c250b70a5bf3b28
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-08 13:37:50 +0530

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo -ne "
-
 -------------------------------------------------------------------------
-                    automated arch linux installer
+            AUTOMATED ARCH LINUX INSTALLER
 -------------------------------------------------------------------------
-             arch-linux-luks-btrfs-grubEncrypt-apparmor-snapper
+            LUKS-BTRFS-GRUB-GRUBPASSWORD-SYSTEMD-APPARMOR-SNAPPER
 -------------------------------------------------------------------------
-
 "
 
 # Find the name of the folder the scripts are in
@@ -21,17 +19,20 @@ set +a
 ( bash $SCRIPTS_DIR/1-presetup.sh )|& tee 1-presetup.log
 source $CONFIGS_DIR/setup.conf
 ( arch-chroot /mnt $HOME/arch-linux/scripts/2-setup.sh )|& tee 2-setup.log
-umount -a
-
 cp -v *.log /mnt/root/arch-linux
 
 echo -ne "
 -------------------------------------------------------------------------
-                    automated arch linux installer
+            AUTOMATED ARCH LINUX INSTALLER
 -------------------------------------------------------------------------
-                        system setup complete
+            ARCH INSTALLATION COMPLETE AND READY TO USE
 -------------------------------------------------------------------------
-                        system is ready to use
--------------------------------------------------------------------------
-
 "
+echo -ne "
+unmounting all file system
+"
+umount -a
+echo -ne "
+rebooting now
+"
+reboot
