0b4b623facaa7cde222e3008b0ef5dadf21f58ad
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-08 19:20:40 +0530

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -15,7 +15,7 @@ CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 PKGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/pkgs
 set +a
 
-( bash $SCRIPTS_DIR/0-startup.sh.sh )|& tee 0-startup.log
+( bash $SCRIPTS_DIR/0-startup.sh )|& tee 0-startup.log
 ( bash $SCRIPTS_DIR/1-presetup.sh )|& tee 1-presetup.log
 source $CONFIGS_DIR/setup.conf
 ( arch-chroot /mnt $HOME/arch-linux/scripts/2-setup.sh )|& tee 2-setup.log
