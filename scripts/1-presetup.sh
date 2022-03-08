a174afd6bcd6172256ac6f8899de3c99ac9dfeb2
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-08 20:38:14 +0530

    Update 1-presetup.sh

--- scripts/1-presetup.sh
+++ scripts/1-presetup.sh
@@ -100,6 +100,7 @@ echo -ne "
 
 pacstrap /mnt $(cat $PKGS_DIR/base-pacstrap) --noconfirm --needed
 cp -R ${SCRIPT_DIR} /mnt/root/arch-linux
+chown root /mnt/root/arch-linux/scripts/2-setup.sh /mnt/root/arch-linux/scripts/3-postsetup.sh
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
 echo -ne "
