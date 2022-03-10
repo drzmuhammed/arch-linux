3485b125b07f804818aa3e24934ef647b94f0961
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-10 23:22:00 +0530

    Update 3-postsetup.sh

--- scripts/3-postsetup.sh
+++ scripts/3-postsetup.sh
@@ -2,6 +2,28 @@
 
 source ${HOME}/arch-linux/configs/setup.conf
 
+echo -ne"
+-------------------------------------------------------------------------
+                    Adding User
+-------------------------------------------------------------------------
+"
+if [ $(whoami) = "root"  ]; then
+    useradd -m -G wheel -s /bin/bash $USERNAME 
+    echo "$USERNAME created, home directory created, added to wheel group, default shell set to /bin/bash"
+
+# use chpasswd to enter $USERNAME:$password
+    echo "$USERNAME:$PASSWORD" | chpasswd
+    echo "$USERNAME password set"
+
+    cp -R $HOME/arch-linux /home/$USERNAME/
+    chown -R $USERNAME: /home/$USERNAME/arch-linux
+    echo "arch-linux copied to home directory"
+
+
+else
+	echo "You are already a user proceed with  installs"
+fi
+
 echo -ne "
 -------------------------------------------------------------------------
             CONFIGURING UFW FIREWALL
@@ -21,33 +43,32 @@ echo -ne "
 -------------------------------------------------------------------------
             Enabling Essential Services
 -------------------------------------------------------------------------
-
 "
 echo -ne "
 enabling acpid.service
 "
-systemctl enable --now acpid
+systemctl enable acpid
 echo -ne "
 enabling  apparmor.service
 "
-systemctl enable --now apparmor
+systemctl enable apparmor
 echo -ne " 
 enabling  auditd.service
 "
-systemctl enable --now auditd
+systemctl enable auditd
 echo -ne "
 enabling  bluetooth.service
 "
-systemctl enable --now bluetooth
+systemctl enable bluetooth
 echo -ne "
 enabling  cpupower.service
 "
-systemctl enable --now cpupower
+systemctl enable cpupower
 
 echo -ne "
 enabling  fail2ban.service
 "
-systemctl enable --now fail2ban
+systemctl enable fail2ban
 
 echo -ne "  
 enabling  fstrim.timer
@@ -57,49 +78,48 @@ systemctl enable fstrim.timer
 echo -ne "  
 enabling  NetworkManager.service
 "
-systemctl enable --now NetworkManager
+systemctl enable NetworkManager
 
 echo -ne "  
 enabling reflector.timer
 "
-systemctl enable --now reflector.timer
+systemctl enable reflector.timer
 
 echo -ne "  
 enabling systemd-remount-fs.service
 "
-systemctl enable --now systemd-remount-fs
+systemctl enable systemd-remount-fs
 
 echo -ne "  
 enabling  systemd-resolved.service
 "
-systemctl enable --now systemd-resolved
+systemctl enable systemd-resolved
 
 echo -ne "  
 enabling  systemd-timesyncd.service
 "
-systemctl enable --now systemd-timesyncd
+systemctl enable systemd-timesyncd
 
 echo -ne "  
 enabling  thermald.service
 "
-systemctl enable --now thermald
+systemctl enable thermald
 
 echo -ne "  
 enabling  tlp.service
 "
-systemctl enable --now tlp
+systemctl enable tlp
 
 echo -ne "  
 enabling  ufw.service
 "
-systemctl enable --now ufw
+systemctl enable ufw
 
 
 timedatectl --no-ask-password set-timezone ${TIME_ZONE}
 timedatectl --no-ask-password set-ntp 1
 
 echo -ne "
-
 -------------------------------------------------------------------------
             Creating Snapper Config
 -------------------------------------------------------------------------
@@ -111,8 +131,19 @@ cp -rfv $HOME/arch-linux/configs/root /etc/snapper/configs/
 mkdir -p /etc/conf.d/
 cp -rfv $HOME/arch-linux/configs/snapper /etc/conf.d/
 
+echo -ne "
+-------------------------------------------------------------------------
+            Configuring Sudo Rights
+-------------------------------------------------------------------------
+"
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+echo -ne "
+-------------------------------------------------------------------------
+            Removing Arch Linux Setup Files
+-------------------------------------------------------------------------
+"
 rm -r $HOME/arch-linux
 rm -r /home/$USERNAME/arch-linux
-
 # Replace in the same state
 cd $pwd
