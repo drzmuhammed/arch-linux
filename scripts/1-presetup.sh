63c80ec1954418d87f477432fd111da333b98491
drzmuhammed <44731028+drzmuhammed@users.noreply.github.com>
2022-03-11 12:06:40 +0530

    Update 1-presetup.sh

--- scripts/1-presetup.sh
+++ scripts/1-presetup.sh
@@ -1,12 +1,61 @@
 #!/usr/bin/env bash
 
+source $SCRIPT_DIR/configs/setup.conf
+
+#SAVING SETUP PARAMETERS
+if ! source $SCRIPT_DIR/configs/setup.conf; then
+	# Loop through user input until the user gives a valid username
+	while true
+	do 
+		read -p "Please enter your username: " username
+		# username regex per response here https://unix.stackexchange.com/questions/157426/what-is-the-regex-to-validate-linux-users
+		# lowercase the username to test regex
+		if [[ "${username,,}" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
+		then 
+			break
+		fi 
+		echo "Incorrect username."
+	done 
+
+    # Loop through user input until the user gives a valid hostname, but allow the user to force save 
+	while true
+	do 
+		read -p "Please name your machine/host name: " name_of_machine
+		# hostname regex (!!couldn't find spec for computer name!!)
+		if [[ "${name_of_machine,,}" =~ ^[a-z][a-z0-9_.-]{0,62}[a-z0-9]$ ]]
+		then 
+			break 
+		fi 
+		# if validation fails allow the user to force saving of the hostname
+		read -p "Hostname doesn't seem correct. Do you still want to save it? (y/n)" force 
+		if [[ "${force,,}" = "y" ]]
+		then 
+			break 
+		fi 
+	done 
+
+    echo "NAME_OF_MACHINE=${name_of_machine,,}" >> $SCRIPT_DIR/configs/setup.conf
+    # convert name to lowercase before saving to setup.conf
+    echo "USERNAME=${username,,}" >> $SCRIPT_DIR/configs/setup.conf
+    #Set luks Password
+    read -p "Please enter your luks password: " luks_password
+    echo "LUKS_PASSWORD=${luks_password,,}" >> $SCRIPT_DIR/configs/setup.conf
+    #Set root Password
+    read -p "Please set root password: " root_password
+    echo "ROOT_PASSWORD=${root_password,,}" >> $SCRIPT_DIR/configs/setup.conf
+    # Set user Password
+    read -p "Please enter your password: " password
+    echo "PASSWORD=${password,,}" >> $SCRIPT_DIR/configs/setup.conf
+
+fi
+
+
 echo -ne "
 ------------------------------------------------------------------------
             PREPARING THE SYSTEM              
 ------------------------------------------------------------------------
 "
 loadkeys us
-source $CONFIGS_DIR/setup.conf
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
 
@@ -47,7 +96,13 @@ echo -ne "
 "
 
 mkfs.fat -F32 ${disk}1
-echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat ${disk}2
+
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << LUKS_CMDS  | cryptsetup -y -v luksFormat ${disk}2
+YES
+${LUKS_PASSWORD}
+${LUKS_PASSWORD}
+LUKS_CMDS
+
 echo -n "${LUKS_PASSWORD}" | cryptsetup luksOpen ${disk}2 cryptedsda2
 mkfs.btrfs -f /dev/mapper/cryptedsda2
 
